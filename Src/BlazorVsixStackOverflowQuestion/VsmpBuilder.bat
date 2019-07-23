853e64763406f40298e91036e33bcc514b7c22aa
Greg Trevellick <gtrevellick@googlemail.com>
2019-07-23 20:41:06 +0100

    .

--- Src/BlazorVsixStackOverflowQuestion/VsmpBuilder.bat
+++ Src/BlazorVsixStackOverflowQuestion/VsmpBuilder.bat
@@ -1,22 +1,10 @@
-cd "C:\_git\AzureDevOpsBlazorSaddlesWidget2\Src\BlazorSaddlesWidget"
+cd "C:\_git\BlazorVsixStackOverflowQuestion\Src\BlazorVsixStackOverflowQuestion"
 
 rem publish the app to \bin....\dist\_framework
 dotnet clean 
-dotnet publish -c Release -o C:\_git\AzureDevOpsBlazorSaddlesWidget2\Src\BlazorSaddlesWidget\bin\Debug\netstandard2.0\publish\
+dotnet publish -c Release -o C:\_git\BlazorVsixStackOverflowQuestion\Src\BlazorVsixStackOverflowQuestion\bin\Debug\netstandard2.0\publish\
 
-rem copy the bin files published above to the app folder
-rem  /S    Copy folders and subfolders
-rem  /Y    Suppress prompt to confirm overwriting a file.
-rem  /F    Display full source and destination file names while copying.
-rem  /R    Overwrite read-only files.
-rem xcopy C:\_git\AzureDevOpsBlazorSaddlesWidget2\Src\BlazorSaddlesWidget\bin\Debug\netstandard2.0\publish\BlazorSaddlesWidget\dist\_framework\* C:\_git\AzureDevOpsBlazorSaddlesWidget2\Src\BlazorSaddlesWidget\wwwroot\_framework\ /F /R /Y /S
-
-rem make the new files read-only
-rem attrib +R C:\_git\AzureDevOpsBlazorSaddlesWidget2\Src\BlazorSaddlesWidget\wwwroot\_framework\*
-
-cd "C:\_git\AzureDevOpsBlazorSaddlesWidget2\Src\BlazorSaddlesWidget\bin\Debug\netstandard2.0\publish\"
+cd "C:\_git\BlazorVsixStackOverflowQuestion\Src\BlazorVsixStackOverflowQuestion\bin\Debug\netstandard2.0\publish\"
 
 rem generate the vsix itself
 tfx extension create --rev-version --manifest-globs vss-extension.json
-
-rem manually upload the vsix into vsmp edit screen (vsmp_pat not used)
---
+++ Src/BlazorVsixStackOverflowQuestion/wwwroot/Images/Overview.md
@@ -0,0 +1,3 @@
+A dashboard widget to share Blazor Saddles with your team.
+
+![](https://raw.githubusercontent.com/GregTrevellick/AzureDevOpsBlazorSaddlesWidget/master/Src/BlazorSaddlesWidget/artefacts/Screenshot.png)
